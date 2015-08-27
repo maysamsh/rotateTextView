aa79e303e779c9e3a091c0579f9921db6479bb69
Maysam Shahsavari <maysam@Maysams-MacBook-Pro.local>
2015-08-27 13:29:29 +0430

    Initial Commit

---
+++ rotateView/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  rotateView
+//
+//  Created by Maysam Shahsavari on 8/27/15.
+//  Copyright (c) 2015 Maysam Shahsavari. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
